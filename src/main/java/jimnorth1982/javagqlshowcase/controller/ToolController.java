236259f825d8fb3f294c0576505b3861277846db
James North <jimnorth82@icloud.com>
2024-06-18 14:19:32 -0400

    Spring beans

---
+++ src/main/java/jimnorth1982/javagqlshowcase/controller/ToolController.java
@@ -0,0 +1,18 @@
+package jimnorth1982.javagqlshowcase.controller;
+
+import jimnorth1982.javagqlshowcase.model.Tool;
+import jimnorth1982.javagqlshowcase.repo.ToolRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+@AllArgsConstructor
+@Controller
+public class ToolController {
+    private final ToolRepository toolRepository;
+    @QueryMapping
+    public Tool findById(@Argument String id) {
+        return null;
+    }
+}
---
+++ src/main/java/jimnorth1982/javagqlshowcase/repo/ToolRepository.java
@@ -0,0 +1,7 @@
+package jimnorth1982.javagqlshowcase.repo;
+
+import jimnorth1982.javagqlshowcase.model.Tool;
+import org.springframework.data.repository.Repository;
+
+interface ToolRepository extends Repository<Tool, String> {
+}
