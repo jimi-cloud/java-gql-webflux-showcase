c07c6636c86152c144816b7c2c53c3b9cbefc40d
James North <jimnorth82@icloud.com>
2024-06-19 16:17:01 -0400

    Added more queries

--- src/main/java/jimnorth1982/javagqlshowcase/controller/ToolController.java
+++ src/main/java/jimnorth1982/javagqlshowcase/controller/ToolController.java
@@ -7,6 +7,8 @@ import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import java.util.List;
+
 @AllArgsConstructor
 @Controller
 public class ToolController {
@@ -15,5 +17,20 @@ public class ToolController {
     @QueryMapping
     public Tool findToolById(@Argument Long id) {
         return toolRepository.findById(id);
+    }
+
+    @QueryMapping
+    public List<Tool> findAllTools() {
+        return toolRepository.findAll();
+    }
+
+    @QueryMapping
+    public List<Tool> findToolsByBrandId(@Argument Long brandId) {
+        return toolRepository.findAllByBrandId(brandId);
+    }
+
+    @QueryMapping
+    public List<Tool> findToolsByToolTypeId(@Argument Long toolTypeId) {
+        return toolRepository.findAllByToolTypeId(toolTypeId);
     }
 }
--- src/main/java/jimnorth1982/javagqlshowcase/repo/ToolRepository.java
+++ src/main/java/jimnorth1982/javagqlshowcase/repo/ToolRepository.java
@@ -1,8 +1,6 @@
 package jimnorth1982.javagqlshowcase.repo;
 
-import jimnorth1982.javagqlshowcase.model.Brand;
 import jimnorth1982.javagqlshowcase.model.Tool;
-import jimnorth1982.javagqlshowcase.model.ToolType;
 import org.springframework.data.repository.Repository;
 
 import java.util.List;
@@ -10,7 +8,9 @@ import java.util.List;
 public interface ToolRepository extends Repository<Tool, Long> {
     Tool findById(Long id);
 
-    List<Tool> getAllByBrand(Brand brand);
+    List<Tool> findAllByBrandId(Long brand);
 
-    List<Tool> getAllByToolType(ToolType toolType);
+    List<Tool> findAllByToolTypeId(Long toolTypeId);
+
+    List<Tool> findAll();
 }
