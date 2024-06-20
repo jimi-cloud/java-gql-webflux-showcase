16481da9ac04d021166494bb5506a0bd4726d688
James North <jimnorth82@icloud.com>
2024-06-20 15:50:11 -0400

    Chores: replace deprecated api

--- src/main/java/jimnorth1982/javagqlshowcase/repo/DateScalarConfiguration.java
+++ src/main/java/jimnorth1982/javagqlshowcase/repo/DateScalarConfiguration.java
@@ -18,50 +18,55 @@ import java.util.Locale;
 public class DateScalarConfiguration {
     @Bean
     public RuntimeWiringConfigurer dateWiring() {
-        var dateScalar = GraphQLScalarType.newScalar()
-                .name("Date")
-                .description("Java 8 LocalDate as scalar.")
-                .coercing(new Coercing<LocalDate, String>() {
-                    @Override
-                    public String serialize(@NonNull final Object dataFetcherResult, @NonNull GraphQLContext context, @NonNull Locale locale) {
-                        if (dataFetcherResult instanceof LocalDate) {
-                            return dataFetcherResult.toString();
-                        } else {
-                            throw new CoercingSerializeException("Expected a LocalDate object.");
-                        }
-                    }
+        return wiringBuilder -> wiringBuilder.scalar(
+                GraphQLScalarType.newScalar()
+                        .name("Date")
+                        .description("Java 8 LocalDate as scalar type for GraphQL Schema.")
+                        .coercing(new Coercing<LocalDate, String>() {
+                                      @Override
+                                      public String serialize(@NonNull final Object dataFetcherResult,
+                                                              @NonNull GraphQLContext context,
+                                                              @NonNull Locale locale) {
+                                          if (dataFetcherResult instanceof LocalDate) {
+                                              return dataFetcherResult.toString();
+                                          } else {
+                                              throw new CoercingSerializeException("Expected a LocalDate object.");
+                                          }
+                                      }
 
-                    @Override
-                    public LocalDate parseValue(@NonNull final Object input, @NonNull GraphQLContext context, @NonNull Locale locale) {
-                        try {
-                            if (input instanceof String) {
-                                return LocalDate.parse((String) input);
-                            } else {
-                                throw new CoercingParseValueException("Expected a String");
-                            }
-                        } catch (DateTimeParseException e) {
-                            throw new CoercingParseValueException(String.format("Not a valid date: '%s'.", input), e
-                            );
-                        }
-                    }
+                                      @Override
+                                      public LocalDate parseValue(@NonNull final Object input,
+                                                                  @NonNull GraphQLContext context,
+                                                                  @NonNull Locale locale) {
+                                          try {
+                                              if (input instanceof String) {
+                                                  return LocalDate.parse((String) input);
+                                              } else {
+                                                  throw new CoercingParseValueException("Expected a String");
+                                              }
+                                          } catch (DateTimeParseException e) {
+                                              throw new CoercingParseValueException(String.format("Not a valid date: '%s'.", input), e
+                                              );
+                                          }
+                                      }
 
-                    @Override
-                    public LocalDate parseLiteral(@NonNull Value<?> input,
-                                                  @NonNull CoercedVariables variables,
-                                                  @NonNull GraphQLContext graphQLContext,
-                                                  @NonNull Locale locale) {
-                        if (input instanceof StringValue) {
-                            try {
-                                return LocalDate.parse(((StringValue) input).getValue());
-                            } catch (DateTimeParseException e) {
-                                throw new CoercingParseLiteralException(e);
-                            }
-                        } else {
-                            throw new CoercingParseLiteralException("Expected a StringValue.");
-                        }
-                    }
-                }).build();
-
-        return wiringBuilder -> wiringBuilder.scalar(dateScalar);
+                                      @Override
+                                      public LocalDate parseLiteral(@NonNull Value<?> input,
+                                                                    @NonNull CoercedVariables variables,
+                                                                    @NonNull GraphQLContext graphQLContext,
+                                                                    @NonNull Locale locale) {
+                                          if (input instanceof StringValue) {
+                                              try {
+                                                  return LocalDate.parse(((StringValue) input).getValue());
+                                              } catch (DateTimeParseException e) {
+                                                  throw new CoercingParseLiteralException(e);
+                                              }
+                                          } else {
+                                              throw new CoercingParseLiteralException("Expected a StringValue.");
+                                          }
+                                      }
+                                  }
+                        ).build()
+        );
     }
 }
