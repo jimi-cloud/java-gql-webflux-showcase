dc5d13ba2fe275085dcb7b555234bf128c4bdad0
James North <jimnorth82@icloud.com>
2024-06-20 10:51:02 -0400

    Add RentalAgreement type and Date scalar type

---
+++ src/main/java/jimnorth1982/javagqlshowcase/repo/DateScalarConfiguration.java
@@ -0,0 +1,59 @@
+package jimnorth1982.javagqlshowcase.repo;
+
+import graphql.language.StringValue;
+import graphql.schema.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+@Configuration
+public class DateScalarConfiguration {
+    @Bean
+    public RuntimeWiringConfigurer dateWiring() {
+        var dateScalar = GraphQLScalarType.newScalar()
+                .name("Date")
+                .description("Java 8 LocalDate as scalar.")
+                .coercing(new Coercing<>() {
+                    @Override
+                    public String serialize(final Object dataFetcherResult) {
+                        if (dataFetcherResult instanceof LocalDate) {
+                            return dataFetcherResult.toString();
+                        } else {
+                            throw new CoercingSerializeException("Expected a LocalDate object.");
+                        }
+                    }
+
+                    @Override
+                    public LocalDate parseValue(final Object input) {
+                        try {
+                            if (input instanceof String) {
+                                return LocalDate.parse((String) input);
+                            } else {
+                                throw new CoercingParseValueException("Expected a String");
+                            }
+                        } catch (DateTimeParseException e) {
+                            throw new CoercingParseValueException(String.format("Not a valid date: '%s'.", input), e
+                            );
+                        }
+                    }
+
+                    @Override
+                    public LocalDate parseLiteral(final Object input) {
+                        if (input instanceof StringValue) {
+                            try {
+                                return LocalDate.parse(((StringValue) input).getValue());
+                            } catch (DateTimeParseException e) {
+                                throw new CoercingParseLiteralException(e);
+                            }
+                        } else {
+                            throw new CoercingParseLiteralException("Expected a StringValue.");
+                        }
+                    }
+                }).build();
+
+        return wiringBuilder -> wiringBuilder.scalar(dateScalar);
+    }
+}
